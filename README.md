019a93edcf998ffc9617b3d703e550088ca28289
JohnnyHu9 <r17d09@gmail.com>
2015-03-14 10:54:39 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Express Portfolio
+
+This is project is a demo project to show Express.js functionality
+
+We used express-generator to create this project
---
+++ public/javascripts/app.js
@@ -0,0 +1,11 @@
+﻿//
+//  File name : app.js
+//  Author's name : Zhixiang Hu
+//  Web site name : Zhixiang Hu Protfolio Site
+//  This is javascript of the web site
+
+function main() {
+    $('.carousel').carousel({
+        interval: 3000
+    });
+ }
